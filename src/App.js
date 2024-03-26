62bc9bc5d682801ebcd7b29848870b3263170dbf
brhmv <allahverdiibrahimov2004@gmail.com>
2024-03-26 16:49:21 +0400

    Add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Users from './Pages/Users/Users';
+import SenderPosts from './Pages/SenderPost/SenderPosts';
+import TravellerPosts from './Pages/TravelerPost/TravellerPosts';
+import SignIn from "./Pages/SignIn/SignIn";
+import Home from "./Pages/Home/Home";
+import Review from "./Pages/Review/Review";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/users" element={<Users />} />
+        <Route path="/TravelerPosts" element={<TravellerPosts />} />
+        <Route path="/SenderPosts" element={<SenderPosts />} />
+        <Route path="/Reviews" element={<Review />} />
+      </Routes>
+    </Router>
   );
 }
 
